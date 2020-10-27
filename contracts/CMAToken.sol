77f8a36bc70affdda4ea7c96af485aa79aabe1c9
Ben Lyaunzon <lyaunzon.b@gmail.com>
2020-10-27 13:53:42 -0700

    switching to 721

--- contracts/CMAToken.sol
+++ contracts/CMAToken.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
-import "@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol";
+import "@openzeppelin/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
-contract CMAToken is ERC1155PresetMinterPauser {
+contract CMAToken is ERC721PresetMinterPauserAutoId {
   constructor()
     public
-    ERC1155PresetMinterPauser(
-      "https://my-json-server.typicode.com/abcoathup/samplenft/tokens/"
+    ERC721PresetMinterPauserAutoId(
+      "Computer Make Art",
+      "CMA",
+      "http://localhost:3000/works/"
     )
   {}
 }
